d284f18ea7e79627c1bf3cfa434796c1124efa32
zhenying tao <Zhenying@zhenyings-MacBook-Pro.local>
2016-06-28 16:25:32 -0400

    modify

---
+++ test.js
@@ -0,0 +1,122 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 3000));
+
+// Server frontpage
+app.get('/', function (req, res) {
+    res.send('This is TestBot Server');
+});
+
+// Facebook Webhook
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+        res.send(req.query['hub.challenge']);
+    } else {
+        res.send('Invalid verify token');
+    }
+});
+
+const StringDecoder = require('string_decoder').StringDecoder;
+const Wit = require('node-wit').Wit
+const http = require('http');
+
+// Wit.ai parameters
+const WIT_TOKEN = 'ZTDH4FZ7T7FWWTFR3Y5CXVYTCBE76OQS';     
+
+// Our bot actions
+const actions = {
+  say(sessionId, context, message, cb) {
+    cb();   
+  },
+  merge(sessionId, context, entities, message, cb) {
+     // Retrieve the location entity and store it into a context field
+    var pathname = '/?qid=1&title=' + encodeURIComponent(message)+ '&body=&category=Knowledge';
+    var options = {
+      host: 'carbonite.mathcs.emory.edu',
+      port: '8080',
+      path:  pathname
+    };
+    console.log(pathname);
+    var decoder = new StringDecoder();
+    http.get(options, (res) => {
+      console.log(`Got response: ${res.statusCode}`);
+      // consume response body
+      res.on('data', function (chunk) {
+        var data = decoder.write(chunk);
+         var beg = data.indexOf("<content>");
+         var end = data.indexOf("</content>");
+         //console.log(data.substring(beg + 9, end));
+         context.answer = data.substring(beg + 9, end);
+          cb(context);
+      });
+      //res.resume();
+    }).on('error', (e) => {
+      console.log(`Got error: ${e.message}`);
+    });
+
+};
+
+// Setting up our bot
+const wit = new Wit(WIT_TOKEN, actions);
+
+
+// handler receiving messages
+app.post('/webhook', function (req, res) {
+    var events = req.body.entry[0].messaging;
+    const context0 = {};
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        // if (event.message && event.message.text) {
+        //     sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        // }
+        if (event.message && event.message.text) {
+            wit.runActions(
+                event.sender.id, // the user's current session
+                event.message.text, // the user's message 
+                context0, // the user's current session state
+                (error, context) => {
+                    console.log("Entering callback");
+                    if (error) {
+                        console.log('Oops! Got an error from Wit:', error);
+                    } else {
+                    // Our bot did everything it has to do.
+                    // Now it's waiting for further messages to proceed.
+                        // console.log('Waiting for futher messages.');
+                        context0 = context;
+                        console.log(context.intro);
+                        console.log("Exiting callback");
+                        //sendMessage(event.sender.id, {text: "reply: "+context0.intro});
+                    }
+                    
+                }
+            );
+        }
+    }
+    res.sendStatus(200);
+});
+
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {    
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
