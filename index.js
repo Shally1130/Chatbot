c7e0e7d7655582984a7c06801665c90ebbac0e2f
zhenying tao <Zhenying@zhenyings-MacBook-Pro.local>
2016-06-29 13:12:39 -0400

    modify

--- index.js
+++ index.js
@@ -168,21 +168,62 @@ app.post('/webhook', function (req, res) {
 });
 
 // generic function sending messages
+// function sendMessage(recipientId, message) {
+//     request({
+//         url: 'https://graph.facebook.com/v2.6/me/messages',
+//         qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+//         method: 'POST',
+//         json: {
+//             recipient: {id: recipientId},
+//             message: message,
+//         }
+//     }, function(error, response, body) {    
+//         if (error) {
+//             console.log('Error sending message: ', error);
+//         } else if (response.body.error) {
+//             console.log('Error: ', response.body.error);
+//         }
+//     });
+// };
+
+
+
 function sendMessage(recipientId, message) {
+  var length = message.length;
+    var num = length/80;
+    for(var i=0;i<num;i++)
+    {
+      request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message.substring(i*80,(i+1)*80-1),
+        }
+      }, function(error, response, body) {    
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+      });
+    }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
         method: 'POST',
         json: {
             recipient: {id: recipientId},
-            message: message,
+            message: message.substring(num*80,length),
         }
-    }, function(error, response, body) {    
+      }, function(error, response, body) {    
         if (error) {
             console.log('Error sending message: ', error);
         } else if (response.body.error) {
             console.log('Error: ', response.body.error);
         }
-    });
+      });
 };
+
 
