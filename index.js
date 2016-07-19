52bdb132b346a364120d91aea75d88a70a3ef7a9
zhenying tao <Zhenying@zhenyings-MacBook-Pro.local>
2016-07-19 17:29:57 -0400

    modify

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-"use strict";
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
@@ -28,7 +27,7 @@ const Wit = require('node-wit').Wit
 const http = require('http');
 
 // Wit.ai parameters
-const WIT_TOKEN = 'WPIDPMNZMWQFK2P2UA7WSKCPKT7MIIXI';     
+const WIT_TOKEN = 'ZTDH4FZ7T7FWWTFR3Y5CXVYTCBE76OQS';     
 
 const firstEntityValue = (entities, entity) => {
   console.log('should be running');
@@ -43,35 +42,17 @@ const firstEntityValue = (entities, entity) => {
   return typeof val === 'object' ? val.value : val;
 };
 
-
-// Wit.ai bot specific code
-
-// This will contain all user sessions.
-// Each session has an entry:
-// sessionId -> {fbid: facebookUserId, context: sessionState}
-const sessions = {};
-
-const findOrCreateSession = (fbid) => {
-  let sessionId;
-  // Let's see if we already have a session for the user fbid
-  Object.keys(sessions).forEach(k => {
-    if (sessions[k].fbid === fbid) { 
-      // Yep, got it!
-      sessionId = k;
-    }
-  });
-  if (!sessionId) {
-    // No session found for user fbid, let's create a new one
-    sessionId = new Date().toISOString();
-    sessions[sessionId] = {fbid: fbid, context: ""};
-  }
-  return sessionId;
-};
+// //system parameter
+// var qid =  '1'; //convert int to char
+// var title;
+// var body='Some%20additional%20information%20on%20the%20question';
+// var category = 'Knowledge'
+// var pathname;
 
 
 // Our bot actions
 const actions = {
-  say(sessionId, context, message, cb) { 
+  say(sessionId, context, message, cb) {
     //console.log(message);
     // var length = context.answer.length;
     // var num = length/310;
@@ -80,19 +61,8 @@ const actions = {
     //   sendMessage(sessionId, {text: "reply: "+(i+1).toString()+'\r\n'+context.answer.substring(i*310,(i+1)*310-1)});
     // }
     //sendMessage(sessionId, {text: "reply: "+(num+1).toString()+'\r\n'+context.answer.substring(num*310,length)});
-
-    const recipientId = sessions[sessionId].fbid;
-    console.log("say....................");
-    if(parseFloat(context.score)>=2.5)
-    {
-      showMoreMessage(recipientId,message,context.url);
-    }
-    else
-    {
-      sendMessage(recipientId,  {text: "reply: "+message});
-    }
-    console.log("message:"+message);
-    //showMoreMessage(sessionId,context.answer,context.url);
+    //sendMessage(sessionId,context.answer);
+    showMoreMessage(sessionId,context.answer,context.url);
     cb();   
   },
   merge(sessionId, context, entities, message, cb) {
@@ -105,9 +75,7 @@ const actions = {
     //   console.log('question!!!!!!!!!!!!!');
     //   //wait.miliseconds(100);    
     // }
-    console.log("merge...................................");
     context.query = message;
-    
     cb(context);
   },
   error(sessionId, context, error) {
@@ -115,22 +83,7 @@ const actions = {
   },
   // You should implement your custom actions here
   // See https://wit.ai/docs/quickstart
-  // ['Greetings']({context, entities}){
-  //   return new Promise(function(resolve, reject) {
-  //   const q = firstEntityValue(entities, 'contact');
-  //   console.log('firstEntityValue');
-  //   // console.log('loc11111111');
-  //   if (q) {
-  //     context.contact = q; 
-  //     console.log('contact!!!!!!!!!!!!!'+contact); 
-  //     //wait.miliseconds(100);    
-  //   }
-  //   return resolve(context); 
-  // });
-  // },
-
   ['Query-Answer'](sessionId, context,cb) {
-
     // Here should go the api call, e.g.:
     // context.forecast = apiCall(context.loc)
     ////////////////////////////////////////////////////
@@ -140,7 +93,6 @@ const actions = {
       port: '8080',
       path:  pathname
     };
-    console.log("api call!!!!!!!!!!!!!!!!!!");
     console.log(pathname);
     var decoder = new StringDecoder();
     http.get(options, (res) => {
@@ -150,24 +102,9 @@ const actions = {
         var data = decoder.write(chunk).trim();
          var beg = data.indexOf("<content>");
          var end = data.indexOf("</content>");
-         var scorebeg = data.indexOf("<confidence>");
-         var scoreend = data.indexOf("</confidence>");
-         var urlbeg = data.indexOf("<resources>");
-         var urlend = data.indexOf("</resources>");
          //console.log(data.substring(beg + 9, end));
-         context.score = data.substring(scorebeg + 12, scoreend);
-         if(parseFloat(context.score)>=2.5)
-         {
-            context.answer = data.substring(beg + 9, end).trim();
-            sessions[sessionId].context += " <answer>" + context.answer+"</answer>";
-            context.url = data.substring(urlbeg + 11, urlend).trim();
-            console.log('score: '+parseFloat(data.substring(scorebeg + 12, scoreend)));
-         }
-         else
-         {
-            context.answer = "Sorry, I don't quite understand your question. Could you say again?";
-            console.log("<2.5................"); 
-         }
+         context.answer = data.substring(beg + 9, end).trim();
+         context.url = 'http://carbonite.mathcs.emory.edu:8080'+pathname;
          //console.log(context.answer);
          cb(context);
       });
@@ -187,48 +124,26 @@ const wit = new Wit(WIT_TOKEN, actions);
 // handler receiving messages
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
-    //console.log("app.post('/webhook', function (req, res) .....................");
-
-    //console.log("Events length = " + events.length);
-    //console.log("Events = " + events);
-    for (var i = 0; i < events.length; i++) {
+    const context0 = {};
+    for (i = 0; i < events.length; i++) {
         var event = events[i];
-        const context0 = {};
         // if (event.message && event.message.text) {
         //     sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
         // }
-
-        ////////////////////////////////////////////////////
-        // We retrieve the Facebook user ID of the sender
-        const sender = event.sender.id;
-
-        // We retrieve the user's current session, or create one if it doesn't exist
-        // This is needed for our bot to figure out the conversation history
-        const sessionId = findOrCreateSession(sender);
-
-        // We retrieve the message content
-        //console.log("Event = " + event);
         if (event.message && event.message.text) {
-            const msg = event.message.text;
-            sessions[sessionId].context += " <question>" + msg + "</question>";
-            /////////////////////////////////////////////////////
-
             wit.runActions(
-                sessionId, // the user's current session
-                msg, // the user's message 
+                event.sender.id, // the user's current session
+                event.message.text, // the user's message 
                 context0, // the user's current session state
                 (error, context) => {
                     console.log("Entering callback");
                     if (error) {
-                        console.log(context);
                         console.log('Oops! Got an error from Wit:', error);
                     } else {
                     // Our bot did everything it has to do.
                     // Now it's waiting for further messages to proceed.
                         // console.log('Waiting for futher messages.');
-                        //console.log("Before answer context = " + context);
-                        //context.answers += "answer:" + context.answer;
-                        console.log("Session content:"+sessions[sessionId].context+"............");
+                        context0 = context;
                         //console.log(context.answer);
                         console.log("Exiting callback");
                         //sendMessage(event.sender.id, {text: "reply: "+context0.intro});
@@ -244,6 +159,47 @@ app.post('/webhook', function (req, res) {
 var messageLeft = "";
 
 
+// //generic function sending messages
+// function sendMessage(recipientId, message) {
+//     var messageLength=310;
+//     if (message.length == 0) return;
+//     //messageLength -= message.substring(0, 310).lastIndexOf(' ');
+//     //console.log('length!!!!!!!!!!!!!!'+messageLength);
+//     // while(message.substring(0, 310).charAt(messageLength)!=' ')
+//     //      messageLength--;    
+//     toSend = message.substring(0, messageLength)
+//     //sendMessage(sessionId, {text: "reply: "+(i+1).toString()+'\r\n'+context.answer.substring(i*310,(i+1)*310-1)});
+//     result={text: "reply: \r\n"+toSend};
+//     console.log(result);
+//     request({
+//       url: 'https://graph.facebook.com/v2.6/me/messages',
+//       qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+//       method: 'POST',
+//       json: {
+//           recipient: {id: recipientId},
+//           message: result,
+//       }
+//     }, function(error, response, body) {    
+//         if (error) {
+//             console.log('Error sending message: ', error);
+//         } else if (response.body.error) {
+//             console.log('Error: ', response.body.error);
+//         }
+//         else
+//         {
+//             if (message.length > messageLength) {
+//               messageLeft = message.substring(messageLength);  // from 310 to the end
+//               sendMessage(recipientId, messageLeft);
+//             }
+//             else
+//             {  
+//               console.log("else!!!!!!!!!!!!!");
+//             }
+            
+//         }
+//     });
+    
+// };
 
 function showMoreMessage(recipientId, text, url) {
   console.log('show more message...........');
@@ -253,13 +209,13 @@ function showMoreMessage(recipientId, text, url) {
                     "type": "template",
                     "payload": {
                         "template_type": "button",
-                        "text":   "reply: "+text.substring(0,40)+'....', 
+                        "text": text.substring(0,40)+'.......', 
                         //"subtitle": "Cute kitten picture",
                         "buttons": [
                           {
                             "type": "web_url",
                             "url": url,
-                            "title": "Show More"
+                            "title": "Show more information"
                           }
                         ]
                     }
@@ -294,53 +250,6 @@ function sendMessage(recipientId, message) {
     });
   console.log('exit send message..................');
 };
-
-//var messageLeft = "";
-
-
-// //generic function sending messages
-// function sendMessage(recipientId, message) {
-//     var messageLength=310;
-//     console.log("sendMessage................");
-//     if (message.length == 0) return;
-//     //messageLength -= message.substring(0, 310).lastIndexOf(' ');
-//     //console.log('length!!!!!!!!!!!!!!'+messageLength);
-//     // while(message.substring(0, 310).charAt(messageLength)!=' ')
-//     //      messageLength--;    
-//     toSend = message.substring(0, messageLength)
-//     //sendMessage(sessionId, {text: "reply: "+(i+1).toString()+'\r\n'+context.answer.substring(i*310,(i+1)*310-1)});
-//     result={text: "reply: \r\n"+toSend};
-//     console.log(result);
-//     request({
-//       url: 'https://graph.facebook.com/v2.6/me/messages',
-//       qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-//       method: 'POST',
-//       json: {
-//           recipient: {id: recipientId},
-//           message: result,
-//       }
-//     }, function(error, response, body) {    
-//         if (error) {
-//             console.log('Error sending message: ', error);
-//         } else if (response.body.error) {
-//             console.log('Error: ', response.body.error);
-//         }
-//         else
-//         {
-//             console.log("message.length.............");
-//             if (message.length > messageLength) {
-//               messageLeft = message.substring(messageLength);  // from 310 to the end
-//               sendMessage(recipientId, messageLeft);
-//             }
-//             else
-//             {  
-//               console.log("else!!!!!!!!!!!!!");
-//             }
-            
-//         }
-//     });
-    
-// };
 
 
 
