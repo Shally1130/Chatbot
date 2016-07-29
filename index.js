3419e180476a8c4fd5fea1d00acc70262d4da739
zhenying tao <Zhenying@zhenyings-MacBook-Pro.local>
2016-07-29 16:01:37 -0400

    modify

--- index.js
+++ index.js
@@ -218,10 +218,8 @@ const actions = {
         //   }
         // });
         const body = {text: "Do you know Panisonic"}; // Given a specific paragraph of text within a document, the Entity Linking Intelligence Service will recognize and identify each separate entity based on the context 
-        var params = {
-            // Request parameters
-        };
-        entityLinking.linkEntity({params,
+        
+        entityLinking.linkEntity({,
                 body
             })
             .then((response) => {
