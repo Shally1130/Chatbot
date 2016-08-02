6a19d431722464f79f8c3425c9accfa82da44fea
zhenying tao <Zhenying@zhenyings-MacBook-Pro.local>
2016-08-02 13:28:47 -0400

    modify

--- index.js
+++ index.js
@@ -253,8 +253,15 @@ const actions = {
         linkEntities(params, (err, result) => {
           if (err) return console.error(err);
           //console.log(JSON.stringify(JSON.parse(result), null, 2));
-          var name = JSON.parse(result).entities[0].name;
-          var wikipediaId = JSON.parse(result).entities[0].wikipediaId;
+          var name = [];
+          var wikipediaId = [];
+          for(int i=0; i<JSON.parse(result).entities.length;i++)
+          {
+          	name.push(JSON.parse(result).entities[i].name);
+          	wikipediaId.push(JSON.parse(result).entities[i].wikipediaId;)
+          }
+          // var name = JSON.parse(result).entities[0].name;
+          // var wikipediaId = JSON.parse(result).entities[0].wikipediaId;
           temp.push(name);
           temp.push(wikipediaId);
           console.log("name: " + name);
