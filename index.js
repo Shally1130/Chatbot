a2856c72bbbbc36a6fb906c844f9f2d7121fc4be
Zhenying <zhenying.tao@emory.edu>
2016-08-17 15:00:15 -0400

    update

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ app.listen((process.env.PORT || 3000));
 
 // Server frontpage
 app.get('/', function (req, res) {
-    res.send('This is TestBot Server'); 
+  res.send('This is TestBot Server'); 
 }); 
 
 // Facebook Webhook
 app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'testbot_verify_token') {
-        res.send(req.query['hub.challenge']);
-    } else {
-        res.send('Invalid verify token');
-    } 
+  if (req.query['hub.verify_token'] === 'testbot_verify_token') {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.send('Invalid verify token');
+  } 
 });  
 
 const StringDecoder = require('string_decoder').StringDecoder;
@@ -43,10 +43,10 @@ const requestHeader = {
 
 const getQueryParams = (params) => {
   return { selection: params.selection, 
-           offset: params.offset }
-}
+    offset: params.offset }
+  }
 
-const linkEntities = (params, callback) => {
+  const linkEntities = (params, callback) => {
   //let queryParams = getQueryParams(params)
   let reqBody = params.text
 
@@ -61,13 +61,13 @@ const linkEntities = (params, callback) => {
 
   request(options, (err, res, body) => {
     if (err) return callback(err)
-    if (!err && res.statusCode !== 200) {
-      return callback(new Error(res.body))
-    }
-    if (!err && res.statusCode === 200) {
-      return callback(null, body)
-    }
-  })
+      if (!err && res.statusCode !== 200) {
+        return callback(new Error(res.body))
+      }
+      if (!err && res.statusCode === 200) {
+        return callback(null, body)
+      }
+    })
 }
 
 // -----------
@@ -79,9 +79,9 @@ const WIT_TOKEN = 'ZTDH4FZ7T7FWWTFR3Y5CXVYTCBE76OQS';
 const firstEntityValue = (entities, entity) => {
   console.log('should be running......................');
   const val = entities && entities[entity] &&
-    Array.isArray(entities[entity]) &&
-    entities[entity].length > 0 &&
-    entities[entity][0].value
+  Array.isArray(entities[entity]) &&
+  entities[entity].length > 0 &&
+  entities[entity][0].value
   ;
   if (!val) {
     return null;
@@ -143,7 +143,7 @@ const actions = {
     else
     {
     	sendMessage(recipientId,  {text: "Feedback: "+message});
-      
+
     }
     if(message=="Do you want more details?")
     {
@@ -192,69 +192,61 @@ const actions = {
     }
     /*parse answer and question*/
     else {
-        console.log("parse question and answer...............");
-        console.log("context.query = " + query);
-        if(query!= null){
-        	pathname = '/?qid=1&title=' + encodeURIComponent(query)+ '&body=Some%20additional%20information%20on%20the%20question&category=Knowledge';
-      		console.log("context.query = "+ query);
-      		console.log("two conditions............"+pathname);
-        }
-      	else{
-      		console.log("the value of yes_no is 'N'");
-      		query = sessions[sessionId].context[size-1][0];
+      console.log("parse question and answer...............");
+      console.log("context.query = " + query);
+      if(query!= null){
+       pathname = '/?qid=1&title=' + encodeURIComponent(query)+ '&body=Some%20additional%20information%20on%20the%20question&category=Knowledge';
+       console.log("context.query = "+ query);
+       console.log("two conditions............"+pathname);
+     }
+     else{
+      console.log("the value of yes_no is 'N'");
+      query = sessions[sessionId].context[size-1][0];
       		//pathname = '/?qid=1&title=' + encodeURIComponent(query)+ '&body=Some%20additional%20information%20on%20the%20question&category=Knowledge';
-	      	pathname += '&badanswer=';
-	      	pathname +=encodeURIComponent(sessions[sessionId].context[size-1][1]);
-	      	console.log("yes_no" + pathname);
+          pathname += '&badanswer=';
+          pathname +=encodeURIComponent(sessions[sessionId].context[size-1][1]);
+          console.log("yes_no" + pathname);
         }
-	  }
-    var options = {
-      host: 'carbonite.mathcs.emory.edu',
-      port: '8080',
-      path:  pathname
-    };
-    console.log("api call!!!!!!!!!!!!!!!!!!");
+      }
+      var options = {
+        host: 'carbonite.mathcs.emory.edu',
+        port: '8080',
+        path:  pathname
+      };
+      console.log("api call!!!!!!!!!!!!!!!!!!");
     //console.log("pathname"+pathname);
     var decoder = new StringDecoder();
-    //Step(
     http.get(options, (res) => {
       console.log(`Got response: ${res.statusCode}`);
       // consume response body
       res.on('data', function (chunk) {
         var data = decoder.write(chunk).trim();
-         var beg = data.indexOf("<content>");
-         var end = data.indexOf("</content>");
-         var scorebeg = data.indexOf("<confidence>");
-         var scoreend = data.indexOf("</confidence>");
-         var urlbeg = data.indexOf("<resources>");
-         var urlend = data.indexOf("</resources>");
-         //console.log(data.substring(beg + 9, end));
-         context.score = data.substring(scorebeg + 12, scoreend);
-         if(parseFloat(context.score)>=2.5){
-         	  delete context.nonAnswer;
-            context.answer = data.substring(beg + 9, end).trim();
-            
-            temp.push(query);
-            temp.push(context.answer);
-            context.url = data.substring(urlbeg + 11, urlend).trim();
-            //console.log('score: '+parseFloat(data.substring(scorebeg + 12, scoreend)));
-         }
-         else{ 
-            delete context.answer;
-            console.log("<2.5................"); 
-         }
+        var beg = data.indexOf("<content>");
+        var end = data.indexOf("</content>");
+        var scorebeg = data.indexOf("<confidence>");
+        var scoreend = data.indexOf("</confidence>");
+        var urlbeg = data.indexOf("<resources>");
+        var urlend = data.indexOf("</resources>");
+        context.score = data.substring(scorebeg + 12, scoreend);
+        if(parseFloat(context.score)>=2.5){
+          delete context.nonAnswer;
+          context.answer = data.substring(beg + 9, end).trim();
+          temp.push(query);
+          temp.push(context.answer);
+          context.url = data.substring(urlbeg + 11, urlend).trim();
+        }
+        else{ 
+          delete context.answer;
+          console.log("<2.5................"); 
+        }
 
         console.log("start linkEntities.................");
 
         let params = {
           text: query+" "+context.answer
         } 
-        //console.log("query+context.answer: "+ params.text);
         linkEntities(params, (err, result) => {
           if (err) return console.error(err);
-          //console.log("query+context.answer: "+ params.text);
-          //console.log("params.selection+params.text"+params.selection+" "+params.text);
-          //console.log(JSON.stringify(JSON.parse(result), null, 2));
           var name = [];
           var wikipediaId = [];
           var pronouns = [];
@@ -266,129 +258,83 @@ const actions = {
           var fNum = ""; //store the id which has the most occurances of female pronouns
           var mNum = ""; //store the id which has the most occurances of male pronouns
           var oNum = ""; //store the id which has the most occurances of object pronouns
-          //console.log(JSON.parse(result));
           console.log("length: " + len);
-          //async.series([
-            //function getInform(callback){
-              //setTimeout( function() { 
-                var forLoop = function(i){
-                  if(i<len){
-                    name.push(JSON.parse(result).entities[i].name);
-                    var tempname = name[i];
-                    console.log("tempname: "+tempname);
-                    wikipediaId.push(JSON.parse(result).entities[i].wikipediaId);
-                    const wikiHost = 'https://en.wikipedia.org/wiki';
-                    let wikiOptions = {
-                    url: wikiHost+'/'+encodeURIComponent(JSON.parse(result).entities[i].wikipediaId),
-                    method: 'POST',
-                    }
-                    request(wikiOptions, (err, res, body) => {
-                      if(err){
-                        console.log("Got an error ",err);
-                      }
-                      else
-                      {
-                        var data = extractor(res.body);
-                        var index = {},
-                        words = data.text.replace(/[.,?!;()"'-]/g, " ").replace(/\s+/g, " ").toLowerCase().split(" ");
-                        index['his'] = 0;
-                        index['he'] = 0;
-                        index['its'] = 0;
-                        index['it'] = 0;
-                        index['she'] = 0;
-                        index['her'] = 0;
-                        //console.log("word: "+words);
-                        words.forEach(function (word) {
-                          if (word==='it'||word==='he'||word==='she'||word==='its'||word==='his'||word==='her') {
-                            index[word]++;
-                            //console.log("index: "+index);
-                          }
-                        });
-                        if((index['it']+index['its'])>oCount)
-                        {
-                          //console.log("index['it']+index['its']>oCount)");
-                          oCount = index['it']+index['its'];
-                          oNum = tempname;
-                          //console.log("it: "+oCount +" "+oNum);
-                        }
-                        if((index['he']+index['his'])>mCount)
-                        {
-                          //console.log("index['his']+index['he']>mCount)");
-                          mCount = index['he']+index['his'];
-                          mNum = tempname;
-                          //console.log("he: "+mCount +" "+mNum);
-                        }
-                        if((index['her']+index['she'])>fCount)
-                        {
-                          //console.log("index['her']+index['she']>fCount)");
-                          fCount = index['her']+index['she'];
-                          fNum = tempname;
-                          // console.log("she: "+fCount +" "+fNum);
-                        }
-                      }
-                      //console.log(res.body);
-                      forLoop(i+1);
-                    })  
-                  }
-                  else {
-                    console.log("storeInform.....................");
-                    //setTimeout( function() { 
-                    console.log("female, male, object: "+fNum+" "+mNum+" "+oNum);
-                    pronouns.push(fNum);
-                    pronouns.push(mNum);
-                    pronouns.push(oNum);
-                    temp.push(name);
-                    temp.push(wikipediaId);
-                    temp.push(pronouns);
-                    console.log("name: " + name);
-                    console.log("wikipediaId: " + wikipediaId);
-                    console.log("pronouns: " + pronouns);
-                    sessions[sessionId].context.push(temp);
-                  } 
+          var forLoop = function(i){
+            if(i<len){
+              name.push(JSON.parse(result).entities[i].name);
+              var tempname = name[i];
+              console.log("tempname: "+tempname);
+              wikipediaId.push(JSON.parse(result).entities[i].wikipediaId);
+              const wikiHost = 'https://en.wikipedia.org/wiki';
+              let wikiOptions = {
+                url: wikiHost+'/'+encodeURIComponent(JSON.parse(result).entities[i].wikipediaId),
+                method: 'POST',
+              }
+              request(wikiOptions, (err, res, body) => {
+                if(err){
+                  console.log("Got an error ",err);
                 }
-                forLoop(0);
-                //callback(null, 'one');
-              //},5000);
-            //},
-            
-            //function storeInform(callback){
-              // console.log("storeInform.....................");
-              // //setTimeout( function() { 
-              //   console.log("female, male, object: "+fNum+" "+mNum+" "+oNum);
-              //   pronouns.push(fNum);
-              //   pronouns.push(mNum);
-              //   pronouns.push(oNum);
-              //   temp.push(name);
-              //   temp.push(wikipediaId);
-              //   temp.push(pronouns);
-              //   console.log("name: " + name);
-              //   console.log("wikipediaId: " + wikipediaId);
-              //   console.log("pronouns: " + pronouns);
-              //   sessions[sessionId].context.push(temp);
-                //callback(null, 'two');
-              //},2000);
-            //}
-        //   ],
-        //    // optional callback
-        //   function(err, results) {
-        //       // results is now equal to ['one', 'two']
-        //       if(err)
-        //         console.log("Got error: "+ err);
-        //     }
-        //   );
-         });
-        
+                else
+                {
+                  var data = extractor(res.body);
+                  var index = {},
+                  words = data.text.replace(/[.,?!;()"'-]/g, " ").replace(/\s+/g, " ").toLowerCase().split(" ");
+                  index['his'] = 0;
+                  index['he'] = 0;
+                  index['its'] = 0;
+                  index['it'] = 0;
+                  index['she'] = 0;
+                  index['her'] = 0;
+                  words.forEach(function (word) {
+                    if (word==='it'||word==='he'||word==='she'||word==='its'||word==='his'||word==='her') {
+                      index[word]++;
+                    }
+                  });
+                  if((index['it']+index['its'])>oCount)
+                  {
+                    oCount = index['it']+index['its'];
+                    oNum = tempname;
+                  }
+                  if((index['he']+index['his'])>mCount)
+                  {
+                    mCount = index['he']+index['his'];
+                    mNum = tempname;
+                  }
+                  if((index['her']+index['she'])>fCount)
+                  {
+                    fCount = index['her']+index['she'];
+                    fNum = tempname;
+                  }
+                }
+                //console.log(res.body);
+                forLoop(i+1);
+              })  
+            }
+            else {
+              console.log("storeInform.....................");
+              console.log("female, male, object: "+fNum+" "+mNum+" "+oNum);
+              pronouns.push(fNum);
+              pronouns.push(mNum);
+              pronouns.push(oNum);
+              temp.push(name);
+              temp.push(wikipediaId);
+              temp.push(pronouns);
+              console.log("name: " + name);
+              console.log("wikipediaId: " + wikipediaId);
+              console.log("pronouns: " + pronouns);
+              sessions[sessionId].context.push(temp);
+            } 
+          }
+          forLoop(0);
+        });
         console.log("end linkEntities.............");
-
-         //console.log(context.answer);
-         cb(context);
+        cb(context);
       });
       res.resume();
-    }).on('error', (e) => {
+      }).on('error', (e) => {
       console.log(`Got error: ${e.message}`);
-    });
-    //cb(context); 
-  },
+      });
+    },
 };
 
 // Setting up our bot
@@ -398,10 +344,6 @@ const wit = new Wit(WIT_TOKEN, actions);
 // handler receiving messages
 app.post('/webhook', function (req, res) {
   var events = req.body.entry[0].messaging;
-  //console.log("app.post('/webhook', function (req, res) .....................");
-
-  //console.log("Events length = " + events.length);
-  //console.log("Events = " + events);
   for (var i = 0; i < events.length; i++) {
     var event = events[i];
     const context0 = {};
@@ -427,8 +369,8 @@ app.post('/webhook', function (req, res) {
         (error, context) => {
           console.log("Entering callback");
           if (error) {
-              console.log(context);
-              console.log('Oops! Got an error from Wit:', error);
+            console.log(context);
+            console.log('Oops! Got an error from Wit:', error);
           } else {
               // Our bot did everything it has to do.
               // Now it's waiting for further messages to proceed.
@@ -439,9 +381,9 @@ app.post('/webhook', function (req, res) {
               //console.log(context.answer);
               console.log("Exiting callback");
               //sendMessage(event.sender.id, {text: "reply: "+context0.intro});
+            }
           }
-        }
-      );
+        );
     }
   }
   res.sendStatus(200);
@@ -459,22 +401,21 @@ function showMoreMessage(recipientId, text, url) {
   	reply = text;
   }
   var message = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "button",
-                        "text":   "Reply: "+ reply,
-                        //"subtitle": "Cute kitten picture",
-                        "buttons": [
-                          {
-                            "type": "web_url",
-                            "url": url,
-                            "title": "Show More"
-                          }
-                        ]
-                    }
-                }
-            };
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "button",
+        "text":   "Reply: "+ reply,
+        "buttons": [
+        {
+          "type": "web_url",
+          "url": url,
+          "title": "Show More"
+        }
+        ]
+      }
+    }
+  };
   console.log('exit show more message...........');
 
   sendMessage(recipientId, message);
@@ -485,21 +426,21 @@ function showMoreMessage(recipientId, text, url) {
 // generic function sending messages
 function sendMessage(recipientId, message) { 
   console.log('send message..................');
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
-    }, function(error, response, body) {    
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {id: recipientId},
+      message: message,
+    }
+  }, function(error, response, body) {    
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
   console.log('exit send message..................');
 };
 
@@ -544,10 +485,10 @@ function sendMessage(recipientId, message) {
 //             {  
 //               console.log("else!!!!!!!!!!!!!");
 //             }
-            
+
 //         }
 //     });
-    
+
 // };
 
 
